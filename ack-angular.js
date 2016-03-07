6ac0f8a2787ffea88a9eee84f8d3fec9a125cca0
ackerapple <acker.dawn.apple@gmail.com>
2016-03-06 23:02:16 -0500

    nide tweaks

--- ack-angular.js
+++ ack-angular.js
@@ -2,7 +2,7 @@ angular.module('ackAngular',[])
 .directive('whiteOutModal',function(){//white-out-modal
   var closeIcon = '<div style="position:relative;opacity:.4;" onmouseover="this.style.opacity=.7" onmouseout="this.style.opacity=.4"><div ng-click="show=false" style="cursor:pointer;font-size:190%;position:absolute;top:0;right:0;margin-right:18px;margin-top:10px;line-height:1;">x</div></div>'
   var table = closeIcon+'<table style="height:100%;" border="0" align="center"><tr><td ng-click="show=false"></td></tr><tr><td ng-transclude></td></tr><tr><td ng-click="show=false"></td></tr></table>'
-  var template = '<div ng-show="show" style="position:fixed;top:0;left:0;z-index:10;height:100%;width:100%;background-color:rgba(255,255,255,.95);overflow:auto;">'+table+'</div>'
+  var template = '<div class="animate-fade" ng-show="show" style="position:fixed;top:0;left:0;z-index:20;height:100%;width:100%;background-color:rgba(255,255,255,.95);overflow:auto;">'+table+'</div>'
   return {
     restrict:'E'
     ,scope:{show:'='}
