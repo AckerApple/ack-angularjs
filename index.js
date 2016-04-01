37c2665cf80709e809cb94620daa8d8dabe8be15
Acker Apple <acker@virgindata.com>
2016-04-01 14:27:21 -0400

    now with ack-x

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//version: 1.0.3
+//version: 1.0.4
 angular.module('ackAngular',['ngAnimate'])
+.service('ackX', require('ackX'))
 .directive('whiteOutModal',function(){//white-out-modal
   return {
     restrict:'E'
