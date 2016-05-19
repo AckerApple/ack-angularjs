bf2bddb6d7f473ab7e6ae9736bbe4404c0245d18
Acker Apple <acker@virgindata.com>
2016-05-19 14:26:45 -0400

    added injector

--- ack-angular.js
+++ ack-angular.js
@@ -180,5 +180,31 @@ function trustAsHtml($sce){
     return $sce.trustAsHtml(text);
   };
 }
-
 trustAsHtml.$inject = ['$sce']
+
+/**
+  Greatly reduces steps required to properly satisfy angular dependency injections
+
+  Example:
+    var inject = injector(MyClass, ['$scope','$http']);
+    function MyClass(){
+      inject(this, arguments)
+      console.log(this.$scope, this.$http)
+    }
+*/
+function injector(jsClass, depArray){
+  return function(ths, args){
+    for(var x=depArray.length-1; x >= 0; --x){
+      var name = depArray[x]
+      if(args[x]){
+        ths[name] = args[x]
+      }
+    }
+  }
+}
+
+
+module.exports = {
+  name:'ack-angular',
+  injector:injector
+}
