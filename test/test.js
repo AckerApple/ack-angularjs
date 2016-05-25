da7221e23bd17c61b8a1848d7cb781bcf913f098
Acker Apple <acker@virgindata.com>
2016-05-25 09:53:23 -0400

    redid injector(args,context)

--- test/test.js
+++ test/test.js
@@ -45157,19 +45157,18 @@
 	  Example:
 	    var inject = injector(MyClass, ['$scope','$http']);
 	    function MyClass(){
-	      inject(this, arguments)
+	      inject(arguments, this)
 	      console.log(this.$scope, this.$http)
 	    }
 	*/
 	function injector(jsClass, depArray){
 	  jsClass.$inject = depArray
-	  return function(ths, args){
+	  return function(args, ths){
+	    ths = ths || {}
 	    for(var x=depArray.length-1; x >= 0; --x){
-	      var name = depArray[x]
-	      if(args[x]){
-	        ths[name] = args[x]
-	      }
+	      if(args[x])ths[ depArray[x] ] = args[x]
 	    }
+	    return ths
 	  }
 	}
 
