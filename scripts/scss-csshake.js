c205f9b49fe3579b11c1f7ac4c4724aa3be1495a
ackerapple <acker.dawn.apple@gmail.com>
2016-07-19 20:25:42 -0400

    csshake

--- scripts/scss-csshake.js
+++ scripts/scss-csshake.js
@@ -7,6 +7,5 @@ var outFilePath = path.join(__dirname,'../','scss','csshake.scss')
 console.log('compiling csshake sass')
 
 module.exports = ackSass.compileFile(filePath, outFilePath, {sourceMap:false})
-.then(()=>ackSass.compileFile(filePath, outFilePath))
 .then(()=>console.log('compiling csshake completed'))
 .catch(err=>console.log(err))
