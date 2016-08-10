a7aeaf91ae71238d8e3c95613ee5fa532e9d1a9b
ackerapple <acker.dawn.apple@gmail.com>
2016-08-10 10:48:45 -0400

    expose AckApi as AckApiControl

--- src/ack-ng-services.module.js
+++ src/ack-ng-services.module.js
@@ -6,4 +6,5 @@ export default angular.module('ack-ng-services', [])
 .service('ack', function(){return ack})
 .service('AckOffline', AckOffline)
 .service('AckApi', AckApi)
+.controller('AckApiControl', AckApi)
 .name
